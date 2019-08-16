38e48bd1b2549707160204ab5267284112498881
Daniel Moniz <daniel.moniz@gmail.com>
2019-08-15 20:01:38 -0400

    Make normal equation prediction. One must be wrong

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -118,7 +118,7 @@ fprintf(['Predicted price of a 1650 sq-ft, 3 br house ' ...
          '(using gradient descent):\n $%f\n'], price);
 
 fprintf('Program paused. Press enter to continue.\n');
-pause;
+% pause;
 
 %% ================ Part 3: Normal Equations ================
 
@@ -147,14 +147,15 @@ X = [ones(m, 1) X];
 theta = normalEqn(X, y);
 
 % Display normal equation's result
-fprintf('Theta computed from the normal equations: \n');
-fprintf(' %f \n', theta);
-fprintf('\n');
+% fprintf('Theta computed from the normal equations: \n');
+% fprintf(' %f \n', theta);
+% fprintf('\n');
 
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+% price = normalEqn(X, y);
+price = theta' * [1; 1650; 3];
 
 
 % ============================================================
