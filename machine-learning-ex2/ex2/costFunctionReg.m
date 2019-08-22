260b6b64b488d11940d783f1e9fbf94b45941e46
Daniel Moniz <daniel.moniz@gmail.com>
2019-08-22 17:58:13 -0400

    Add cost J to costFunctionReg

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -8,7 +8,12 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
+thetaX = X * theta;
+h = sigmoid(thetaX);
+extra_reg_cost = (lambda / (2*m)) * sum(theta .^ 2);
+cost_sum = sum((-y * log(h)') - (1 - y) * (log(1 - h)'));
+J = (1/m) * cost_sum + extra_reg_cost;
+
 grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
