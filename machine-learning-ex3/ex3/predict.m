23f7ad89e7884efe0477ec3d891736319cbb7aa0
Daniel Moniz <daniel.moniz@gmail.com>
2019-08-24 12:24:42 -0400

    Complete neural network forward propagations

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -8,7 +8,15 @@ m = size(X, 1);
 num_labels = size(Theta2, 1);
 
 % You need to return the following variables correctly 
-p = zeros(size(X, 1), 1);
+% p = zeros(size(X, 1), 1);
+
+A1 = [ones(m, 1), X];
+A2 = [ones(m, 1), sigmoid(Theta1 * A1')'];
+A3 = sigmoid(Theta2 * A2')';
+
+[_, p] = max(A3, [], 2);
+
+
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
