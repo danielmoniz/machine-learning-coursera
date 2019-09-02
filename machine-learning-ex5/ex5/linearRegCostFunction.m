f4d30f47169e5acab8d6d3d97f568a34b558792f
Daniel Moniz <daniel.moniz@gmail.com>
2019-09-02 12:12:42 -0400

    Complete ex5 linear regression cost (unsubmitted)

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -19,6 +19,10 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+cost = sum((X * theta - y) .^ 2);
+regularization = sum(lambda * (theta(2:end, :) .^ 2));
+J = 1 / (2 * m) * (cost + regularization);
+
 
 
 
